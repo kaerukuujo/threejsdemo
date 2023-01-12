e470f7fd3a7f548d8313fc72fa7af22e9505be99
kaerukuujo <mrjolyne@gmail.com>
2023-01-12 18:52:34 +0000

    added earth mesh + shaders, working mouse movement

--- threejsdemo/main.js
+++ threejsdemo/main.js
@@ -1,4 +1,9 @@
+import gsap from 'gsap';
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl';
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -9,12 +14,78 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer (
-);
+  {
+    antialias: true,
+});
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+//create a sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50), 
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./img/globe.jpg'),
+      },
+    } 
+  })
+);
+
+//create atmosphere
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50), 
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending, 
+    side: THREE.BackSide
+  })
+);
+atmosphere.scale.set(1.1, 1.1, 1.1);
+
+scene.add(atmosphere);
+
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group)
+
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+});
+
+const stars = new THREE.Points()
+
+camera.position.z = 15;
+
+const mouse = {
+  x: undefined,
+  y: undefined
+};
 
 function animate(){
   requestAnimationFrame(animate);
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
+  sphere.rotation.y += 0.003;
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.3,
+    y: mouse.x * 0.5,
+    duration: 2,
+  });
 };
 animate();
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth)
+    * 2 - 1;
+  mouse.y = (event.clientY / innerHeight)
+    * 2 + 1;
+
+  // console.log(mouse);
+});
+
+
